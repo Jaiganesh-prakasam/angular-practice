2882df4e29a16cec2dd1e078f47ef8f7c2e8ac49
Jai ganesh <jaiganesh.prakasam@gmail.com>
2020-03-08 16:45:57 +0530

    Experiment with ng class and class

--- src/app/in-built-directives/ngif-ngfor-ngclass/ngif-ngfor-ngclass.component.ts
+++ src/app/in-built-directives/ngif-ngfor-ngclass/ngif-ngfor-ngclass.component.ts
@@ -10,28 +10,28 @@ export class NgifNgforNgclassComponent implements OnInit {
     {
       companyName: "Hyundai",
       models: [
-        { modelName: "i10", color: "red" },
-        { modelName: "120", color: "green" },
-        { modelName: "elantra", color: "yellow" },
-        { modelName: "verna", color: "blue" }
+        { modelName: "i10", color: "red", status: true },
+        { modelName: "120", color: "green", status: false },
+        { modelName: "elantra", color: "yellow", status: true },
+        { modelName: "verna", color: "blue", status: false }
       ]
     },
     {
       companyName: "Honda",
       models: [
-        { modelName: "city", color: "red" },
-        { modelName: "civic", color: "green" },
-        { modelName: "camry", color: "yellow" },
-        { modelName: "fortuner", color: "blue" }
+        { modelName: "city", color: "red", status: false },
+        { modelName: "civic", color: "green", status: true },
+        { modelName: "camry", color: "yellow", status: true },
+        { modelName: "fortuner", color: "blue", status: false }
       ]
     },
     {
       companyName: "Maruthi-Suzuki",
       models: [
-        { modelName: "swift", color: "red" },
-        { modelName: "wagonR", color: "green" },
-        { modelName: "ertiga", color: "yellow" },
-        { modelName: "baleno", color: "blue" }
+        { modelName: "swift", color: "red", status: false },
+        { modelName: "wagonR", color: "green", status: true },
+        { modelName: "ertiga", color: "yellow", status: true },
+        { modelName: "baleno", color: "blue", status: true }
       ]
     }
   ];
