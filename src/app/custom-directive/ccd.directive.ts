13f2ddc28087fa8f157436492cb3f13b287474ea
Jai ganesh <jaiganesh.prakasam@gmail.com>
2020-03-26 13:18:56 +0530

    HostBinding HostListener
    * Capture mouseOver mouseLeave event using HostListener
    * Directive ccd bind nativeElement using renderer2
    * HostBinding to hilight borders

--- src/app/custom-directive/ccd.directive.ts
+++ src/app/custom-directive/ccd.directive.ts
@@ -1,10 +1,29 @@
-import { Directive, ElementRef, Renderer2 } from "@angular/core";
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  HostListener,
+  HostBinding
+} from "@angular/core";
 
 @Directive({
   selector: "[appCcd]"
 })
 export class CcdDirective {
+  @HostBinding("style.border.px") ishovering = "solid 3";
   constructor(private el: ElementRef, private renderer: Renderer2) {
-    renderer.setStyle(el.nativeElement, "backgroundColor", "gray");
+    // renderer.setStyle(el.nativeElement, "backgroundColor", "gray");
+  }
+  @HostListener("mouseover") onMouseOver() {
+    this.ishovering = "0";
+    console.log("mouse over");
+    let part = this.el.nativeElement.querySelector(".card-text");
+    this.renderer.setStyle(part, "backgroundColor", "white");
+  }
+  @HostListener("mouseleave") onMouseLeave() {
+    this.ishovering = "solid 4";
+    console.log("mouse leave");
+    let part = this.el.nativeElement.querySelector(".card-text");
+    this.renderer.setStyle(part, "backgroundColor", "white");
   }
 }
