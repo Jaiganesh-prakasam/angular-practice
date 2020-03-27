6cb023b460ed26f857e3881f9cfaca157299a148
Jai ganesh <jaiganesh.prakasam@gmail.com>
2020-03-27 15:52:41 +0530

    Reactive form example
    use of observables in reactive form
    Acheive the same output without using reactive forms

--- src/app/reactive-programing/reactive-programing.module.ts
+++ src/app/reactive-programing/reactive-programing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ObservableSubscribeComponent } from "./observable-subscribe/observable-subscribe.component";
-
+import { FormExampleComponent } from "./form-example/form-example.component";
+import { ReactiveFormsModule } from "@angular/forms";
 @NgModule({
-  declarations: [ObservableSubscribeComponent],
-  imports: [CommonModule],
-  exports: [ObservableSubscribeComponent]
+  declarations: [ObservableSubscribeComponent, FormExampleComponent],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [ObservableSubscribeComponent, FormExampleComponent]
 })
 export class ReactiveProgramingModule {}
