038980a8dafc73776880f0125c6715e76a7626fc
Jai ganesh <jaiganesh.prakasam@gmail.com>
2020-04-03 21:09:23 +0530

    service registered at component.provider and component.viewProvider

--- src/app/dependency-injection/dependency-injection.module.ts
+++ src/app/dependency-injection/dependency-injection.module.ts
@@ -11,7 +11,7 @@ import { FormsModule } from "@angular/forms";
   declarations: [ParentComponent, ChildComponent],
   imports: [FormsModule, CommonModule],
   providers: [
-    SimpleService
+    // SimpleService
     // InjectInjectableService
     // ProvidersExampleService
     // InjectorExampleService
--- src/app/dependency-injection/parent/parent.component.ts
+++ src/app/dependency-injection/parent/parent.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 import { SimpleService } from "../simple.service";
+
 @Component({
   selector: "parent",
   templateUrl: "./parent.component.html",
-  styleUrls: ["./parent.component.sass"]
+  styleUrls: ["./parent.component.sass"],
+  // providers: [SimpleService]
+  viewProviders: [SimpleService]
 })
 export class ParentComponent implements OnInit {
   constructor(public service: SimpleService) {}
