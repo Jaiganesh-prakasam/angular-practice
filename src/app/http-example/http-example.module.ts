6a3007afe4764984379802ccdad0a80d2c3e6457
Jai ganesh <jaiganesh.prakasam@gmail.com>
2020-04-09 13:22:21 +0530

    Jsonp to fetch data from iTunes search service

--- src/app/http-example/http-example.module.ts
+++ src/app/http-example/http-example.module.ts
@@ -1,24 +1,34 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CoreHttpComponent } from "./core-http/core-http.component";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HttpClientJsonpModule } from "@angular/common/http";
 import { HttpUsingPromiseComponent } from "./http-using-promise/http-using-promise.component";
 import { SearchService } from "./search.service";
 import { SearchObservableService } from "./search-observable.service";
+import { SearchJsonPService } from "./search-json-p.service";
 import { HttpUsingObservableComponent } from "./http-using-observable/http-using-observable.component";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { HttpUsingJsonpComponent } from "./http-using-jsonp/http-using-jsonp.component";
 @NgModule({
   declarations: [
     CoreHttpComponent,
     HttpUsingPromiseComponent,
     HttpUsingObservableComponent,
+    HttpUsingJsonpComponent,
   ],
-  imports: [CommonModule, HttpClientModule, ReactiveFormsModule, FormsModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientJsonpModule,
+  ],
   exports: [
     CoreHttpComponent,
     HttpUsingPromiseComponent,
     HttpUsingObservableComponent,
+    HttpUsingJsonpComponent,
   ],
-  providers: [SearchService, SearchObservableService],
+  providers: [SearchService, SearchObservableService, SearchJsonPService],
 })
 export class HttpExampleModule {}
