3804618b3c57548539733f0a91b90c74b58b1c35
Jai ganesh <jaiganesh.prakasam@gmail.com>
2020-04-15 14:18:41 +0530

    Parameterised router
    Search operation is done only when search button is clicked
    parameter is sent along with the route
    Parameter is fetched using activated route

--- src/app/router-config/search-json-p.service.ts
+++ src/app/router-config/search-json-p.service.ts
@@ -6,14 +6,17 @@ import { map } from "rxjs/operators";
 @Injectable()
 export class SearchJsonPService {
   apiRoot: string = "https://itunes.apple.com/search";
-
-  constructor(private http: HttpClient) {}
+  results: SearchItem[];
+  constructor(private http: HttpClient) {
+    this.results = [];
+  }
 
   search(term: string) {
     let apiURL = `${this.apiRoot}?term=${term}&media=music&limit=20`;
+    this.results = [];
     return this.http.jsonp(apiURL, "callback").pipe(
       map((res) => {
-        return (res as any).results.map((item) => {
+        return (this.results = (res as any).results.map((item) => {
           return new SearchItem(
             item.trackName,
             item.artistName,
@@ -21,7 +24,7 @@ export class SearchJsonPService {
             item.artworkUrl30,
             item.artistId
           );
-        });
+        }));
       })
     );
   }
--- src/app/router-config/search/search.component.ts
+++ src/app/router-config/search/search.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl } from "@angular/forms";
 import { Observable } from "rxjs";
-import {
-  debounceTime,
-  distinctUntilChanged,
-  tap,
-  switchMap,
-} from "rxjs/operators";
+// import {
+//   debounceTime,
+//   distinctUntilChanged,
+//   tap,
+//   switchMap,
+// } from "rxjs/operators";
 import { SearchItem } from "../data-model/search-item.model";
 import { SearchJsonPService } from "../search-json-p.service";
+import { Router, ActivatedRoute } from "@angular/router";
 @Component({
   selector: "app-search",
   templateUrl: "./search.component.html",
@@ -18,19 +19,42 @@ export class SearchComponent implements OnInit {
   public loading: boolean = false;
   public results: Observable<SearchItem[]>;
   public searchField: FormControl;
-  constructor(private itunes: SearchJsonPService) {
+  constructor(
+    public itunes: SearchJsonPService,
+    public router: Router,
+    private route: ActivatedRoute
+  ) {
     this.searchField = new FormControl();
-    this.results = this.searchField.valueChanges.pipe(
-      debounceTime(400),
-      distinctUntilChanged(),
-      tap((_) => (this.loading = true)),
-      switchMap((term) => this.itunes.search(term)),
-      tap((_) => (this.loading = false))
-    );
+
+    // to do search on changes
+    this.route.params.subscribe((params) => {
+      console.log(params);
+      if (params["term"]) {
+        this.doSearch(params["term"]);
+      }
+    });
+
+    // // reactive way to fetch data
+    // this.results = this.searchField.valueChanges.pipe(
+    //   debounceTime(400),
+    //   distinctUntilChanged(),
+    //   tap((_) => (this.loading = true)),
+    //   switchMap((term) => this.itunes.search(term)),
+    //   tap((_) => (this.loading = false))
+    // );
   }
 
   ngOnInit(): void {}
+  //
   doSearch(term: string) {
-    this.itunes.search(term);
+    console.log(term);
+    this.loading = true;
+    this.itunes
+      .search(term)
+      .toPromise()
+      .then((_) => (this.loading = false));
+  }
+  onSearch(term: string) {
+    this.router.navigate(["search", { term: term }]);
   }
 }
